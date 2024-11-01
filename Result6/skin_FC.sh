7fa6be57e1e4f59399e69eebd2aa7b404e0a69d1
liusai118 <1543916185@qq.com>
2024-11-01 18:56:01 +0800

    Add files via upload

---
+++ Result6/skin_FC.sh
@@ -0,0 +1,28 @@
+output=/home/liusai/24/space/result/workplace/skin
+source /home/liusai/miniconda3/bin/activate rna
+# Corrected "ouput" to "output"
+mkdir -p $output/fastp
+mkdir -p $output/sam
+mkdir -p $output/bam
+
+
+cat skin.txt | while read id
+do
+    id=${id%%.*}
+    cat index.txt | while read type
+    do
+    mkdir -p $output/FC/${type}
+    mkdir -p $output/sam/${type}/${id}
+    bowtie2 -t -p 60 -x /home/liusai/24/space/tempindex/skin/${type}/${type} \
+    -1 $input/${id}.1.fq  \
+    -2 $input/${id}.2.fq  \
+    -S $output/sam/${type}/${id}/${id}.sam
+    samtools view -@10 -bS $output/sam/${type}/${id}/${id}.sam > $output/FC/${type}/${id}_bowtie2.bam
+    done
+
+done
+cat index.txt | while read type
+do
+     featureCounts -T 10 -a /home/liusai/24/space/tempindex/skin/${type}/${type}.gtf -o ${type}read.count -p -t exon -g gene_id  $output/FC/${type}/*.bam
+done
+
