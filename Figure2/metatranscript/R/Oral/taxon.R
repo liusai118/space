a71056462da4ab05070c453bae0c09cd65f061a6
liusai118 <1543916185@qq.com>
2025-03-12 18:39:38 +0800

    Add files via upload

---
+++ Figure2/metatranscript/R/Oral/taxon.R
@@ -0,0 +1,16 @@
+taxonomy1 <- read.csv("./taxon.csv")
+taxonomy2 <-taxonomy1[,8:2]
+rownames(taxonomy2) <- taxonomy2$species
+colnames(taxonomy2) <- c( "Kingdom" ,"Phylum"  ,"Class" ,  "Order" ,  "Family",  "Genus" ,   "Species")
+library(microeco)
+taxonomy3 <- taxonomy2[!taxonomy2$Kingdom=="0",]
+taxonomy3 <- taxonomy3[!taxonomy3$Phylum=="0",]
+taxonomy3 <- taxonomy3[!taxonomy3$Family=="0",]
+taxonomy3 <- taxonomy3[!taxonomy3$Genus=="environmental samples",]
+taxonomy3 <- taxonomy3[!taxonomy3$Species=="0",]
+taxonomy3 <- taxonomy3[!taxonomy3$Order=="0",]
+taxonomy3 <- taxonomy3[!taxonomy3$Class=="0",]
+taxonomy3 <- taxonomy3[!taxonomy3$Genus=="0",]
+taxonomy3 <- tidy_taxonomy(taxonomy3)
+taxonomy3 <- taxonomy3[!taxonomy3$Genus=="g__",]
+taxonomy3<- taxonomy3[rownames(taxonomy3) %in% rownames(otutab),]
